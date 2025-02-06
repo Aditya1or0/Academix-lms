061aa7d2f58029103fb5392d67e410428644d65c
Aditya1or0 <adityapandit264@gmail.com>
2025-02-06 20:15:37 +0530

    updated context file

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -34,7 +34,7 @@ export const AppContextProvider = ({ children }) => {
 
   const calculateChapterTime = (chapter) => {
     let time = 0;
-    chapter.chapterContent.map((lecture) => (time += lectureDuration));
+    chapter.chapterContent.map((lecture) => (time += lecture.lectureDuration));
     return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] });
   };
 
